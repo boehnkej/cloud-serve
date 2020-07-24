3c6732de08c93b9f18ac6fd609dd478bbf595913
Jana Böhnke <boehnke@gmx.de>
2020-07-24 16:57:04 +0200

    Add test setup

---
+++ tests/sitetest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if curl http://serve:5000 | grep ">site"; then
+    echo "Site exits.\nSite test passes.\n"
+    exit 0
+else
+    echo "Site does not exist.\nSite test fails."    
+    exit 1
+fi
+    
